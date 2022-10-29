33d18b33651550c9deb72451b9efec5caa06a7c6
zaidanammar <zaidanammar23@gmail.com>
2022-10-30 01:47:27 +0700

    :zap: test ssr not working

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,8 @@
+import { useState } from 'react'
 import type { AppProps } from 'next/app'
 import { QueryClientProvider, QueryClient, Hydrate } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import '../styles/globals.css'
-import { useState } from 'react'
 
 const App = ({ Component, pageProps }: AppProps) => {
   const [queryClient] = useState(() => new QueryClient())
--- pages/market.tsx
+++ pages/market.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/destructuring-assignment */
 import React from 'react'
 import { dehydrate, QueryClient } from '@tanstack/react-query'
 
@@ -6,17 +7,21 @@ import ContainerMarket from 'containers/market'
 import MetaHeader from '@/molecules/meta-header'
 import { Market } from '@/constants/seo'
 
-const Page = () => (
-  <>
-    <MetaHeader
-      title={Market.title}
-      description={Market.description}
-      keywords={Market.keywords}
-      image={Market.image}
-    />
-    <ContainerMarket />
-  </>
-)
+const Page = (props: any) => {
+  const dataTradePriceChanges = props?.dehydratedState?.queries[0]?.state?.data?.data
+
+  return (
+    <>
+      <MetaHeader
+        title={Market.title}
+        description={Market.description}
+        keywords={Market.keywords}
+        image={Market.image}
+      />
+      <ContainerMarket />
+    </>
+  )
+}
 
 export const getServerSideProps = async () => {
   const queryClient = new QueryClient()
@@ -27,12 +32,30 @@ export const getServerSideProps = async () => {
   )
   await queryClient.fetchQuery(['trade-price-changes'], () => getTradePriceChanges())
 
+  // const data = await getTradePriceChanges()
   return {
     props: {
+      // data,
       // dehydrate query cache
       dehydratedState: dehydrate(queryClient),
     },
   }
 }
+// export const getServerSideProps = async () => {
+//   const queryClient = new QueryClient()
+
+//   //   prefetch data on the server
+//   await queryClient.fetchQuery(['wallet-supported-currencies'], () =>
+//     getWalletSupportedCurrencies(),
+//   )
+//   await queryClient.fetchQuery(['trade-price-changes'], () => getTradePriceChanges())
+
+//   return {
+//     props: {
+//       // dehydrate query cache
+//       dehydratedState: dehydrate(queryClient),
+//     },
+//   }
+// }
 
 export default Page
